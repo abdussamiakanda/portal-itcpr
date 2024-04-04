c4aa54006da7f4460d4b214eba9c9cc653ac1648
abdussamiakanda <abdussamiakanda@gmail.com>
2024-04-04 02:41:34 -0500

    .

--- status/status.js
+++ status/status.js
@@ -1,3 +1,15 @@
 function goToExternal(path) {
   window.open(path, "_blank");
 }
+
+function updateContent() {
+  fetch('http://10.144.162.80/status.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('content').innerHTML = data;
+    })
+    .catch(console.error);
+}
+
+// Poll for changes every 5 seconds
+setInterval(updateContent, 1000);

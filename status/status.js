d8aa347d6b17ddc4a879fb10188215c02fc10d36
abdussamiakanda <abdussamiakanda@gmail.com>
2024-04-04 02:50:06 -0500

    .

--- status/status.js
+++ status/status.js
@@ -3,7 +3,7 @@ function goToExternal(path) {
 }
 
 function updateContent() {
-  
+  fetch('http://10.144.162.80/status.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('status').innerHTML = data;
